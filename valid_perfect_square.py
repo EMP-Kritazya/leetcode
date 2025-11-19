ca29d2d53a99392858b214f2893bbaae10efe1fc
EMP-Kritazya <emp.kritazya@gmail.com>
2025-11-19 12:16:40 -0600

    took some thinking; had to use the ideas of index while doing binary search. But in this problems index is the actual value

---
+++ valid_perfect_square.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isPerfectSquare(self, num: int) -> bool:
+        l, r = 0, num
+        
+        while l<r:
+            m = l + ((r-l)//2)
+            
+            if m*m == num:
+                return True
+            elif m*m > num:
+                r = m - 1
+            else:
+                l = m+1
+        
+        if l*l == num:
+            return True
+        else:
+            return False
