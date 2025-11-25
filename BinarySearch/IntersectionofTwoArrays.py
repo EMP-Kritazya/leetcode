0bf9cf113457f6d25405b87fc53b6085a3d7c357
EMP-Kritazya <emp.kritazya@gmail.com>
2025-11-25 11:31:09 -0600

    Easy

---
+++ BinarySearch/IntersectionofTwoArrays.py
@@ -0,0 +1,31 @@
+class Solution:
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        result = []
+        mini = nums1 if len(nums1)<len(nums2) else nums2
+        maxi = nums1 if mini == nums2 else nums2
+        mini.sort()
+        maxi.sort()
+        
+        
+        for i in mini:
+            a = self.BinarySearch(i, maxi)
+            if a!=-1 and a not in result:
+                result.append(a)
+        
+        return result
+        
+    def BinarySearch(self, i, maxi):
+        l, r = 0, len(maxi)-1
+        
+        while l < r:
+            m = l + ((r-l)//2)
+            
+            if maxi[m] > i:
+                r = m-1
+            elif maxi[m]<i:
+                l = m + 1
+            else:
+                maxi.pop(m)
+                return i
+        
+        return i if maxi[l] == i else -1
